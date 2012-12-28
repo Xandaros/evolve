6c1b12697774bd0746263d0a9e1f5ccd3efb6e1b
Xandaros <mz.bremerhaven@googlemail.com>
2012-12-28 13:48:45 +0100

    Added command to change gamemode

--- lua/ev_plugins/sh_changemap.lua
+++ lua/ev_plugins/sh_changemap.lua
@@ -6,21 +6,35 @@ local PLUGIN = {}
 PLUGIN.Title = "Change Map"
 PLUGIN.Description = "Change the map."
 PLUGIN.Author = "Overv"
-PLUGIN.ChatCommand = "map"
-PLUGIN.Usage = "<map>"
-PLUGIN.Privileges = { "Map changing" }
+PLUGIN.ChatCommand = {"map", "gamemode"}
+PLUGIN.Usage = {"<map>", "<gamemode>"}
+PLUGIN.Privileges = { "Map changing", "Gamemode changing" }
 
-function PLUGIN:Call( ply, args )
-	if ( ply:EV_HasPrivilege( "Map changing" ) ) then
-		if ( args[1] and file.Exists( "maps/" .. args[1] .. ".bsp", "GAME" ) ) then
-			evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has changed the map to ", evolve.colors.red, args[1], evolve.colors.white, "." )
-			
-			timer.Simple( 0.5, function() RunConsoleCommand( "changelevel", args[1] ) end )
+function PLUGIN:Call( ply, args, arg, cmd )
+	if cmd == "map" then
+		if ( ply:EV_HasPrivilege( "Map changing" ) ) then
+			if ( args[1] and file.Exists( "maps/" .. args[1] .. ".bsp", "GAME" ) ) then
+				evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has changed the map to ", evolve.colors.red, args[1], evolve.colors.white, "." )
+				
+				timer.Simple( 0.5, function() RunConsoleCommand( "changelevel", args[1] ) end )
+			else
+				evolve:Notify( ply, evolve.colors.red, "Specified map not found!" )
+			end
 		else
-			evolve:Notify( ply, evolve.colors.red, "Specified map or gamemode not found!" )
+			evolve:Notify( ply, evolve.colors.red, evolve.constants.notallowed )
 		end
-	else
-		evolve:Notify( ply, evolve.colors.red, evolve.constants.notallowed )
+	elseif cmd == "gamemode" then
+		if ply:EV_HasPrivilege("Gamemode changing") then
+			if (args[1] and file.Exists("gamemodes/" .. args[1], "GAME")) then
+				evolve:Notify(evolve.colors.blue, ply:Nick(), evolve.colors.white, " has changed the gamemode to ", evolve.colors.red, args[1], evolve.colors.white, ".")
+				
+				timer.Simple(0.5, function() RunConsoleCommand("gamemode", args[1]) end)
+			else
+				evolve:Notify(ply, evolve.colors.red, "Specified gamemode not found!")
+			end
+		else
+			evolve:Notify(ply, evolve.colors.red, evolve.constants.notallowed)
+		end
 	end
 end
 
