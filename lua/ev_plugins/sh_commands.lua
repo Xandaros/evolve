853116faee62dd74b53abd2e77c22f48a2e240e2
Xandaros <mz.bremerhaven@googlemail.com>
2012-12-30 21:04:11 +0100

    Fix commands plugin. Fixes #13

--- lua/ev_plugins/sh_commands.lua
+++ lua/ev_plugins/sh_commands.lua
@@ -13,29 +13,46 @@ function PLUGIN:Initialize()
 end
 
 function PLUGIN:Call( ply, args )
-	local commands = table.Copy( evolve.plugins )
+	local commands = {}
 	--table.SortByMember( commands, "ChatCommand", function( a, b ) return a > b end )
 	
 	if ( ply:IsValid() ) then
+		for _, plug in ipairs( evolve.plugins ) do
+			if ( plug.ChatCommand ) then
+				if type(plug.ChatCommand) == "string" then
+					plug.ChatCommand = { plug.ChatCommand }
+				else
+					for k, ChatCommand in ipairs( plug.ChatCommand ) do
+						local usage
+						
+						if (type(plug.Usage) == "table") then
+							usage = plug.Usage[k]
+						else
+							usage = plug.Usage
+						end
+						
+						table.insert(commands, {ChatCommand, tostring(usage), plug.Description})
+					end
+				end
+			end
+		end
+		
+		table.SortByMember(commands, 1, function(a,b) return a > b end)
+		
 		net.Start( "EV_Command" )
 			net.WriteBit( false )
 			net.WriteString( "\n============ Available chat commands for Evolve ============\n" )
 		net.Send( ply )
 		
-		for _, plug in ipairs( commands ) do
-			if ( plug.ChatCommand ) then
-				if type(plug.ChatCommand) == "string" then plug.ChatCommand = { plug.ChatCommand } end
-				for __, ChatCommand in ipairs( plug.ChatCommand ) do
-					net.Start( "EV_Command" )
-						net.WriteBit( true )
-						net.WriteString( ChatCommand )
-						net.WriteString( tostring( plug.Usage ) )
-						net.WriteString( plug.Description )
-					net.Send( ply )
-				end
-
-			end
+		for k,cmd in pairs(commands) do
+			net.Start( "EV_Command" )
+				net.WriteBit( true )
+				net.WriteString( cmd[1] )
+				net.WriteString( cmd[2] )
+				net.WriteString( cmd[3] )
+			net.Send( ply )
 		end
+		
 		net.Start( "EV_Command" )
 			net.WriteBit( false )
 			net.WriteString( "" )
@@ -55,20 +72,22 @@ function PLUGIN:Call( ply, args )
 	end
 end
 
-net.Receive( "EV_Command", function( len )
-	if net.ReadBit() == 1 then
-		local com   = net.ReadString()
-		local usage = net.ReadString()
-		local desc  = net.ReadString()
-		
-		if usage != "nil" then
-			print( "!" .. com .. " " .. usage .. " - " .. desc )
+if CLIENT then
+	net.Receive( "EV_Command", function( len )
+		if net.ReadBit() == 1 then
+			local com   = net.ReadString()
+			local usage = net.ReadString()
+			local desc  = net.ReadString()
+			
+			if usage != "nil" then
+				print( "!" .. com .. " " .. usage .. " - " .. desc )
+			else
+				print( "!" .. com .. " - " .. desc )
+			end
 		else
-			print( "!" .. com .. " - " .. desc )
+			print( net.ReadString() )
 		end
-	else
-		print( net.ReadString() )
-	end
-end)
+	end)
+end
 
 evolve:RegisterPlugin( PLUGIN )
