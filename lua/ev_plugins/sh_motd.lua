b86ce47e04d2cd55370848fe57db3bc7f6197f00
Xandaros <mz.bremerhaven@googlemail.com>
2013-09-09 14:15:28 +0200

    Added Trainfuck and MOTD by divran
    Probably needs a way to set MOTD in-game. For now the file needs to be created and edited manually
    
    Closes #71

---
+++ lua/ev_plugins/sh_motd.lua
@@ -0,0 +1,99 @@
+/*-------------------------------------------------------------------------------------------------------------------------
+	Message of the Day
+-------------------------------------------------------------------------------------------------------------------------*/
+
+local PLUGIN = {}
+PLUGIN.Title = "MOTD"
+PLUGIN.Description = "Message of the Day."
+PLUGIN.Author = "Divran"
+PLUGIN.ChatCommand = "motd"
+PLUGIN.Usage = nil
+PLUGIN.Privileges = nil
+
+function PLUGIN:Call(ply, args)
+	self:OpenMotd(ply)
+end
+
+function PLUGIN:PlayerInitialSpawn(ply)
+	timer.Simple(1, function() ply:ConCommand("evolve_startmotd") end)
+end
+
+function PLUGIN:OpenMotd(ply)
+	if (SERVER) then
+		ply:ConCommand("evolve_motd")
+	end
+end
+
+if (SERVER) then 
+	if (file.Exists("evolvemotd.txt", "DATA")) then
+		resource.AddFile("data/evolvemotd.txt")
+	end
+
+	for k,v in pairs(player.GetAll()) do
+		v:ConCommand("evolve_startmotd")
+	end
+end
+
+
+if (CLIENT) then
+	function PLUGIN:CreateMenu()
+		self.StartPanel = vgui.Create("DFrame")
+		local w,h = 150,50
+		self.StartPanel:Center()
+		self.StartPanel:SetSize(w, h)
+		self.StartPanel:SetTitle("Welcome!")
+		self.StartPanel:SetVisible(false)
+		self.StartPanel:SetDraggable(true)
+		self.StartPanel:ShowCloseButton(false)
+		self.StartPanel:SetDeleteOnClose(false)
+		self.StartPanel:SetScreenLock(true)
+		self.StartPanel:MakePopup()
+		
+		self.OpenButton = vgui.Create("DButton", self.StartPanel)
+		self.OpenButton:SetSize(150 / 2 - 4, 20)
+		self.OpenButton:SetPos(2, 25)
+		self.OpenButton:SetText("Open MOTD")
+		self.OpenButton.DoClick = function()
+			PLUGIN.MotdPanel:SetVisible(true)
+			PLUGIN.StartPanel:SetVisible(false) 
+		end
+		
+		self.CloseButton = vgui.Create("DButton",self.StartPanel)
+		self.CloseButton:SetSize(150 / 2 - 6, 20)
+		self.CloseButton:SetPos(150 / 2 + 4, 25)
+		self.CloseButton:SetText("Close")
+		self.CloseButton.DoClick = function() 
+			PLUGIN.StartPanel:SetVisible(false) 
+		end
+		
+		self.MotdPanel = vgui.Create("DFrame")
+		local w,h = ScrW() - 200,ScrH() - 200
+		self.MotdPanel:SetPos(100, 100)
+		self.MotdPanel:SetSize(w, h)
+		self.MotdPanel:SetTitle("MOTD")
+		self.MotdPanel:SetVisible(false)
+		self.MotdPanel:SetDraggable(false)
+		self.MotdPanel:ShowCloseButton(true)
+		self.MotdPanel:SetDeleteOnClose(false)
+		self.MotdPanel:SetScreenLock(true)
+		self.MotdPanel:MakePopup()
+		
+		self.MotdBox = vgui.Create("HTML", self.MotdPanel)
+		self.MotdBox:StretchToParent(4, 25, 4, 4)
+		self.MotdBox:SetHTML(file.Read("evolvemotd.txt"))
+	end
+	
+	concommand.Add("evolve_motd", function(ply,cmd,args)
+		PLUGIN.MotdPanel:SetVisible(true)
+	end)
+	
+	concommand.Add("evolve_startmotd", function(ply,cmd,args)
+		if file.Exists("evolvemotd.txt", "DATA") then
+			if not PLUGIN.StartPanel then PLUGIN:CreateMenu() end
+			PLUGIN.StartPanel:SetVisible(true)
+		end
+	end)
+	
+end
+
+evolve:RegisterPlugin(PLUGIN)
---
+++ lua/ev_plugins/sh_trainfuck.lua
@@ -0,0 +1,58 @@
+/*-------------------------------------------------------------------------------------------------------------------------
+	Fuck a player with a train
+-------------------------------------------------------------------------------------------------------------------------*/
+
+local PLUGIN = {}
+PLUGIN.Title = "Trainfuck"
+PLUGIN.Description = "Trainfuck a player."
+PLUGIN.Author = "Divran"
+PLUGIN.ChatCommand = "trainfuck"
+PLUGIN.Usage = "[players]"
+PLUGIN.Privileges = { "trainfuck" }
+PLUGIN.Trains = 0
+
+function PLUGIN:Call( ply, args )
+	if ( ply:EV_HasPrivilege( "trainfuck" ) ) then
+		local players = evolve:FindPlayer( args, ply )
+		
+		for _, pl in ipairs( players ) do
+			pl:SetMoveType( MOVETYPE_WALK )
+			self:SpawnTrain( pl:GetPos() + pl:GetForward() * 1000 + Vector(0,0,50), pl:GetForward() * -1 )
+		end
+		
+		if ( #players > 0 ) then
+			evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has trainfucked ", evolve.colors.red, evolve:CreatePlayerList( players ), evolve.colors.white, "." )
+		else
+			evolve:Notify( ply, evolve.colors.red, "No matching players found." )
+		end
+	else
+		evolve:Notify( ply, evolve.colors.red, evolve.constants.notallowed )
+	end
+end
+
+function PLUGIN:SpawnTrain( Pos, Direction )
+	local train = ents.Create( "prop_physics" )
+	train:SetModel("models/props_vehicles/train_boxcar.mdl")
+	train:SetAngles( Direction:Angle() )
+	train:SetPos( Pos )
+	train:Spawn()
+	train:Activate()
+	train:EmitSound( "ambient/alarms/train_horn2.wav", 100, 100 )
+	local phys = train:GetPhysicsObject()
+	if (phys) then
+		phys:SetVelocity( Direction * 50000 )
+	end
+	
+	timer.Create("TrainRemove_"..PLUGIN.Trains, 5, 1, function() train:Remove() end)
+	PLUGIN.Trains = PLUGIN.Trains + 1
+end
+
+function PLUGIN:Menu( arg, players )
+	if ( arg ) then
+		RunConsoleCommand( "ev", "trainfuck", unpack( players ) )
+	else
+		return "Trainfuck", evolve.category.punishment
+	end
+end
+
+evolve:RegisterPlugin( PLUGIN )
