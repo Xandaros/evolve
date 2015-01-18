6394d50b175fcae31a8183776061e3a123f90a26
Martin Zeller <mz.bremerhaven@gmail.com>
2015-01-18 04:12:43 +0100

    Update vON and remove it from global namespace
    Using this opportunity to move all files to a new directory.
    These will be incompatible, so you will need to use a script to convert
    them.
    
    Fix #125
    Ref #119

--- lua/autorun/server/ev_autorun.lua
+++ lua/autorun/server/ev_autorun.lua
@@ -12,7 +12,12 @@ AddCSLuaFile( "ev_cl_init.lua" )
 AddCSLuaFile( "ev_menu/cl_menu.lua" )
 
 // Load serverside files
-include( "von.lua" )
+--Load vON
+local von_bak = von
+include( "../../includes/ev_vON/von.lua")
+evolve.von = von
+von = von_bak
+
 include( "ev_framework.lua" )
 include( "ev_sv_init.lua" )
 include( "ev_menu/sv_menu.lua" )
