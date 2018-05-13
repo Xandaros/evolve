f943e7634e07bdf2d004d273bcd56aafc9482c2a
stazio <staz@staz.io>
2018-05-13 11:32:34 -0400

    Properly implemented the menu. Fixed simple typos and used a real boolean for toggling.

--- lua/ev_menu/cl_menu.lua
+++ lua/ev_menu/cl_menu.lua
@@ -38,6 +38,8 @@ function MENU:TabSelected( tab )
 end
 
 function MENU:Initialize()
+	self.IsVisible = false
+
 	self.Panel = vgui.Create( "DFrame" )
 	self.Panel:SetSize( 260, 490 )
 	self.Panel:SetPos( -self.Panel:GetWide(), ScrH() / 2 - self.Panel:GetTall() / 2 )
@@ -121,6 +123,7 @@ function MENU:Show()
 			timer.Destroy( "EV_MenuShow" )
 		end
 	end )
+	self.IsVisible = true
 end
 
 function MENU:Hide()
@@ -137,17 +140,14 @@ function MENU:Hide()
 			timer.Destroy( "EV_MenuShow" )
 		end
 	end )
-end
-
-function MENU:IsShown()
-	return self.Panel	
+	self.IsVisible = false
 end
 
 function MENU:Toggle() 
-	if (self.IsShown())
-		MENU:Hide()	
+	if self.IsVisible then
+		self:Hide()	
 	else
-		MENU:Show()	
+		self:Show()	
 	end
 end
 
